8ec2c9818851adff9053d0f2e1391ccebdb431f8
ehdwns1082 <ehdwns1082@naver.com>
2020-06-11 20:05:32 +0900

    .

--- 멈춤보단 천천히라도 OpenCV python 강좌/Chapter(1)_load_image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-image = cv2.imread("python_logo.png")
-
-cv2.imshow("Image",image)
-cv2.waitKey(0)
-
-
-
--- 잡동사니 탐구 OpenCV python 강좌/연습ㅈ아.py
+++
@@ -1,7 +0,0 @@
-
-print('x%d'%3)
-print(type('x%d'%3))
-print(type('x%d'%3))
-print('str')
-str = '123'
-print(int(str))
